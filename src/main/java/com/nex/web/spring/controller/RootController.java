57d481a790599e7e9655075bd7f8a9486d3bf123
lukas treml <lukas.treml@t-systems.cz>
2014-03-07 13:18:32 +0100

    build repaired

--- src/main/java/com/nex/web/spring/controller/RootController.java
+++ src/main/java/com/nex/web/spring/controller/RootController.java
@@ -1,10 +1,7 @@
 package com.nex.web.spring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.nex.domain.localized.Type;
 
 @Controller
 @RequestMapping("/")
